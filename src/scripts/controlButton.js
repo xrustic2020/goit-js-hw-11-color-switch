1d628593c0cfb99ec592cd4f17a9afbd119d5f28
Tisanyuk Ruslan <ruslantisanykot@gmail.com>
2020-12-03 20:57:59 +0200

    release 2.0, del isActive and added disabled button

--- src/scripts/controlButton.js
+++ src/scripts/controlButton.js
@@ -3,20 +3,17 @@ import colors from '../data/colors';
 import refs from './refs';
 
 let intervalId = null;
-let isStarting = false;
 
 const handleStart = function () {
-  if (!isStarting) {
-    isStarting = true;
-    intervalId = setInterval(() => {
-      refs.body.style.backgroundColor =
-        colors[randomIntegerFromInterval(0, colors.length)];
-    }, 1000);
-  }
+  refs.startButton.disabled = true;
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length)];
+  }, 1000);
 };
 
 const handleStop = function () {
-  isStarting = false;
+  refs.startButton.disabled = false;
   clearInterval(intervalId);
 };
 
